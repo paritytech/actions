93afc14616ca959276db0a0f70097d91592a3d6e
Wilfried Kopp <wilfried@parity.io>
2023-04-18 14:57:50 +0200

    WIP
    - replaced version by tag
    - fix script

--- release/artifacts/upload/lib.sh
+++ release/artifacts/upload/lib.sh
@@ -1,11 +1,49 @@
 #!/usr/bin/env bash
 
+echoerr() { echo "$@" 1>&2; }
+
+validate_mode() {
+    case $1 in
+        standard)
+        #"standard")
+        #"standard")
+        return 0
+        ;;
+
+        *)
+        return 1
+        ;;
+
+    esac
+}
+
 check_mode_inputs () {
     MODE=$1
-    [ -z "$MODE" ] && echo "The mode is mandatory"; return 1
+    if [[ -z "$MODE" ]]; then
+        echoerr "The mode is mandatory"
+        return 1
+    fi
 
-    echo "Checking mode $MODE"
+    if [[ $(validate_mode ${MODE}) -ne 0 ]]; then
+        echoerr "Invalid mode: $MODE"
+        return 1
+    fi
 
-    echo "Not implemented yet"
+    echo "Checking mode: $MODE"
+
+
+    case $MODE in
+        standard)
+            [[ -z $PRODUCT ]] && { echoerr "Missing ENV: PRODUCT"; return 1; }
+            [[ -z $TAG ]] && { echoerr "Missing ENV: TAG"; return 1; }
+            # [[ -z $ARCH ]] && { echoerr "Missing ENV: ARCH"; return 1; }
+            return 0
+        ;;
+
+        *)
+        echoerr "unreachable"
+        ;;
+    esac
+
     return 1
 }
