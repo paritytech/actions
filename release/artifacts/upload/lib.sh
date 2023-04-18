f75eb88883520ae74b5ff3c945ee109ba6a9bbdb
Wilfried Kopp <wilfried@parity.io>
2023-04-18 15:57:17 +0200

    WIP: Refactoring

--- release/artifacts/upload/lib.sh
+++ release/artifacts/upload/lib.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+shopt -s expand_aliases
+VOL=/data
+alias rs="docker run --rm -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_REGION -e AWS_BUCKET -v $PWD:$VOL $IMAGE"
+alias awscli="docker run --rm  -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_REGION -e AWS_BUCKET -e AWS_CONFIG_FILE paritytech/awscli"
+
 echoerr() { echo "$@" 1>&2; }
 
+# Check if the mode is one of the supported ones
 validate_mode() {
     case $1 in
-        standard)
-        #"standard")
-        #"standard")
+        standard|gha)
+        # release)
         return 0
         ;;
 
@@ -15,24 +20,20 @@ validate_mode() {
         ;;
 
     esac
+    return 1
 }
 
 # Check if the mode is valid and if we have all
 # the ENV required for it
 check_mode_inputs () {
     MODE=$1
-    if [[ -z "$MODE" ]]; then
-        echoerr "The mode is mandatory"
-        return 1
-    fi
+    [[ -z "$MODE" ]] && { echoerr "The mode is mandatory"; return 1; }
 
-    if [[ $(validate_mode ${MODE}) -ne 0 ]]; then
+    validate_mode "${MODE}"
+    if [[ $? -ne 0 ]]; then
         echoerr "Invalid mode: $MODE"
         return 1
     fi
-
-    echo "Checking mode: $MODE"
-
 
     case $MODE in
         standard)
@@ -43,10 +44,28 @@ check_mode_inputs () {
         ;;
 
         *)
-            echoerr "unreachable"
+            echoerr "unreachable case"
             return 1
         ;;
     esac
 
+    echoerr "unreachable"
     return 1
+}
+
+# Call ls on the S3 bucket to check for content
+check_s3 () {
+    # TODO: we don't want to see ALL
+    # echo "Check: https://${AWS_BUCKET}.s3.${AWS_DEFAULT_REGION}.amazonaws.com/index.html"
+    awscli aws s3 ls ${AWS_BUCKET} --summarize
+    awscli aws s3 ls ${AWS_BUCKET}/${PRODUCT} --human-readable --recursive
+}
+
+show_versions () {
+    echo "Running rs image version: $(rs version)"
+    echo "Running awscli image version: $(awscli aws --version)"
+}
+
+main() {
+    echo "TODO"
 }
